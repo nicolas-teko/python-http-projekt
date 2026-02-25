52a2f4b72f7ce133327509da9a89813fb309a494
nicolas-teko <nicolas.wirth@edu.teko.ch>
2026-02-25 14:05:18 +0100

    1st Pull, Comit & Push

---
+++ Code/Yves/test_selenium.py
@@ -0,0 +1,5 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+service_object = Service('C:\Users\Yves_\PycharmProjects\python-http-projekt\Code\Yves\edgedriver_win64')
