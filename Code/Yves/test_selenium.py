c6cc65fc74c61681f98a4e6919c2f5c9066de1ba
y95weber <yves_weber@hotmail.ch>
2026-02-25 16:10:02 +0100

    1st Pull, Comit & Push

--- Code/Yves/test_selenium.py
+++ Code/Yves/test_selenium.py
@@ -1,5 +1,25 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
 
-service_object = Service('C:\Users\Yves_\PycharmProjects\python-http-projekt\Code\Yves\edgedriver_win64')
+# Webseite öffnen
+driver = webdriver.Edge()
+driver.get('https://www.youtube.com')
+sleep(3)
+
+
+# POST für suche starten
+search_bar = driver.find_element(By.NAME, 'search_query')
+search_bar.send_keys('Selenium Tutorial')
+# POST für Button Search klick
+button_click = driver.find_element(By.CLASS_NAME, 'ytSearchboxComponentSearchButton')
+button_click.click()
+sleep(3)
+
+# Titel vom Header anzeigen
+titel_seite = driver.title
+print(f"Seitentitel: {titel_seite}")
+
+
+input("Drücke Enter zum Beenden...")
+driver.quit()
